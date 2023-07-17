244d793e35bd63473ded8dc8294a42b21ada2d45
Miguel <miguel-sg01@hotmail.com>
2023-07-17 00:20:21 -0500

    feat: workflow deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://dylasx.github.io',
+  base: '/astro-portfolio',
+  integrations: [tailwind()],
 });
